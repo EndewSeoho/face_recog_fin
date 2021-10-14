5b3f019c7011e171d14e21766df09e1509c21372
EndewSeoho <pureencounter@gmail.com>
2021-10-14 18:27:28 +0900

    asd

--- face_recog_api/models.py
+++ face_recog_api/models.py
@@ -7,8 +7,8 @@ from django.db import models
 
 class FaceRecogApi(models.Model):
     no = models.AutoField(db_column='NO', primary_key=True)  # Field name made lowercase.
-    image = models.CharField(db_column='IMAGE', max_length=100)  # Field name made lowercase.
-    id = models.CharField(db_column='ID', max_length=45)  # Field name made lowercase.
+    image = models.CharField(db_column='IMAGE', max_length=1000)  # Field name made lowercase.
+    id = models.CharField(db_column='ID', max_length=100)  # Field name made lowercase.
     gender = models.IntegerField(db_column='GENDER', blank=True, null=True)  # Field name made lowercase.
     age = models.IntegerField(db_column='AGE', blank=True, null=True)  # Field name made lowercase.
     company = models.CharField(db_column='COMPANY', max_length=100)  # Field name made lowercase.
@@ -16,4 +16,5 @@ class FaceRecogApi(models.Model):
     class Meta:
         managed = False
         db_table = 'FACE_RECOG_API'
+
 
